1a29bad3bb70edd74e6b29f7a90a44462762a2f1
ablashika <kofoworola203@gmail.com>
2023-06-29 18:27:51 +0000

    ended at type assertion

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 // console.log("Hello world!");
-//changing ECMA with --target
 function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
 }
 greet("Maddison", new Date());
+// let obj: any = { x: 0 };
+// // None of the following lines of code will throw compiler errors.
+// // Using `any` disables all further type checking, and it is assumed 
+// // you know the environment better than TypeScript.
+// obj.foo();
+// obj();
+// obj.bar = 100;
+// obj = "hello";
+// const n: number = obj;
+var myName = "Alice";
+var names = ["Alice", "Bob", "Eve"];
+// Contextual typing for function - parameter s inferred to have type string
+names.forEach(function (s) {
+    console.log(s.toUpperCase());
+});
+// Contextual typing also applies to arrow functions
+names.forEach(function (s) {
+    console.log(s.toUpperCase());
+});
+// Object Types
+function printName(obj) {
+    return obj;
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
--- index.ts
+++ index.ts
@@ -4,3 +4,67 @@ function greet(person: string, date: Date) {
 }
  
 greet("Maddison", new Date())
+
+// let obj: any = { x: 0 };
+// // None of the following lines of code will throw compiler errors.
+// // Using `any` disables all further type checking, and it is assumed 
+// // you know the environment better than TypeScript.
+// obj.foo();
+// obj();
+// obj.bar = 100;
+// obj = "hello";
+// const n: number = obj;
+
+const namess = ["Alice", "Bob", "Eve"];
+ 
+// Contextual typing for function - parameter s inferred to have type string
+names.forEach(function (s) {
+  console.log(s.toUpperCase());
+});
+ 
+// Contextual typing also applies to arrow functions
+namess.forEach((s) => {
+  console.log(s.toUpperCase());
+});
+
+// Object Types
+
+function printName(obj: { first: string; last?: string }) {
+  return obj
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+
+// Union type.
+// instead of 
+function printIds(id: number | string) {
+  // console.log(id.toUpperCase());
+// Property 'toUpperCase' does not exist on type 'string | number'.
+//   Property 'toUpperCase' does not exist on type 'number'.
+}
+
+// rather
+function printId(id: number | string) {
+  if (typeof id === "string") {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
+
+
+//one key difference between the type alias type and inferrence is A type cannot be changed after being created
+
+//type assertion
+// Type assertion is used to indicate to the compiler 
+// that you have performed a type check yourself 
+// and are certain about the type of a value. 
+// It is important to note that type assertions do not perform any 
+// runtime type checks or data conversion. 
+// They are purely a way to provide hints to the TypeScript 
+// compiler during static analysis.
+let myVariable: any = "Hello, TypeScript!";
+let lengths: number = (<string>myVariable).length;
