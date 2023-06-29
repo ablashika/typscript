7863c758c68e27451735dbe9e10b5a4f2954c4f4
ablashika <kofoworola203@gmail.com>
2023-06-29 13:07:53 +0000

    downleveling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // console.log("Hello world!");
+//changing ECMA with --target
 function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 greet("Maddison", new Date());
